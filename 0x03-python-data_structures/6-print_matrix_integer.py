08b7ae36ce5f8594bed2a5fc97728cabad2b84a4
Nomoya-Magazi <105003971+Nomoya-Magazi@users.noreply.github.com>
2022-08-28 19:41:44 +0200

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
-def no_c(my_string):
-    new_string = my_string.translate({ord(i): None for i in 'cC'})
-    return new_string
+
+def print_matrix_integer(matrix=[[]]):
+    """
+    A function that prints a matrix of integers.
+    """
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end="")
+            if j != (len(matrix[i]) - 1):
+                print(" ", end="")
+
+        print("")
