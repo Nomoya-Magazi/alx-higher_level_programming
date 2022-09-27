7f2a5a015c6cc643eb3e8b2d9e23bba370a80858
Nomoya-Magazi <105003971+Nomoya-Magazi@users.noreply.github.com>
2022-09-27 11:24:22 +0200

    Update 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""an object attribute lookup function"""
+"""Defines an object attribute lookup function"""
+
 
 def lookup(obj):
     """Returns the list of avaliable attributes and methods of an object"""
