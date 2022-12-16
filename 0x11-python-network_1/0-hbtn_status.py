3549cd35b82f2760bc328dfb9f03bd77fae948e1
Nomoya-Magazi <105003971+Nomoya-Magazi@users.noreply.github.com>
2022-12-16 15:21:48 +0200

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-"""This python module contains a script that fetches
-https://alx-intranet.hbtn.io/status"""
-from urllib.request import urlopen
+"""
+    Module fetches https://intranet.hbtn.io/status
+"""
+import urllib.request
 
 
 if __name__ == "__main__":
-    with urlopen('https://alx-intranet.hbtn.io/status') as response:
-        content = response.read()
-    print('Body response:')
-    print('\t- type: {}'.format(type(content)))
-    print('\t- content: {}'.format(content))
-    print('\t- utf8 content: {}'.format(content.decode('utf8')))
+    try:
+        with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
+            if resp is not None:
+                html = resp.read()
+                print("Body response:")
+                print("\t- type: {}".format(type(html)))
+                print("\t- content: {}".format(html))
+                print("\t- utf8 content: {}".format(html.decode('utf-8')))
+    except Exception:
+        pass
